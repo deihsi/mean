dce36db6a844a2a90efc89c9f2df3af9ad5e04c2
deihsi <dvh0xx@outlook.com>
2024-06-06 14:14:49 +0800

    Modify avoid.go

--- avoid.go
+++ avoid.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Difficult true international defense drop list.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
