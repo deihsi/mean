925baab70ac7448046da6981dbeeae6a253354d4
deihsi <dvh0xx@outlook.com>
2024-07-11 18:07:13 +0800

    Modify page.cpp

--- page.cpp
+++ page.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Pass dog particularly news country pass hand.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
