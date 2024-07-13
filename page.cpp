c1018d407918bdb15436f24fc451c32ff77b7331
deihsi <dvh0xx@outlook.com>
2024-07-13 14:22:45 +0800

    Modify page.cpp

--- page.cpp
+++ page.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Bank from view even artist.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
