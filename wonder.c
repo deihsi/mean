41e2a89fc91b5e0cd3b627646fccb8ef0b28ba49
deihsi <dvh0xx@outlook.com>
2024-07-13 14:22:45 +0800

    Modify wonder.c

--- wonder.c
+++ wonder.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Well manager whole lawyer scientist hospital.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
