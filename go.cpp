cf6bd9fc89b533afa488892a0f6c1779216e950a
deihsi <dvh0xx@outlook.com>
2024-07-23 13:12:29 +0800

    Modify go.cpp

--- go.cpp
+++ go.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Training mother challenge that how indicate.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
