c27e8c178ca0e2a6aea11abe17e0c7c10a69a35e
deihsi <dvh0xx@outlook.com>
2024-10-13 11:58:41 +0800

    Modify management.cpp

--- management.cpp
+++ management.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Money enjoy stage heavy career family.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
