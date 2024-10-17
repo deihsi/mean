185d7b0f80066558f8c3f115ea2ce8f1b7e14fba
deihsi <dvh0xx@outlook.com>
2024-10-17 10:38:12 +0800

    Modify management.cpp

--- management.cpp
+++ management.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# History tell better administration effort door business.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
