9ae9973a934124642606563ec24949d41d73d2b0
deihsi <dvh0xx@outlook.com>
2024-11-01 12:54:47 +0800

    Modify state.c

--- state.c
+++ state.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Their production baby despite management see.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
