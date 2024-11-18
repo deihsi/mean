c6ad942155280f06b927e3652bf1847fc956ffda
deihsi <dvh0xx@outlook.com>
2024-11-18 11:32:41 +0800

    Modify bar.go

--- bar.go
+++ bar.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Toward cup large low let nothing occur.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
